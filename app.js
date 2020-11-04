f372515c0e71988a1c8ccfddc093f160565f8f73
disensee <disensee27@gmail.com>
2020-11-04 16:08:41 -0600

    Add EJS and create first template

--- app.js
+++ app.js
@@ -1,10 +1,46 @@
 const express = require('express');
-const bodyParser = require(body-parser);
+const bodyParser = require('body-parser');
 
-const app = espress();
+const app = express();
+app.set('view engine', 'ejs');
 
 app.get("/", (req, res) => {
-  res.send("Hello");
+  var today = new Date();
+  var currentDay = today.getDay();
+  var day = "";
+
+  switch (currentDay) {
+    case 0:
+      day = "Sunday";
+      break;
+    case 1:
+      day = "Monday";
+      break;
+    case 2:
+      day = "Tuesday";
+      break;
+    case 3:
+      day = "Wednesday";
+      break;
+    case 4:
+      day = "Thursday";
+      break;
+    case 5:
+      day = "Friday";
+      break;
+    case 6:
+      day = "Saturday";
+      break;
+    default:
+    console.log("ERROR: Current Day is equal to: " + currentDay);
+
+  }
+  
+  res.render('list', {
+    day: day
+  });
 });
 
-app.listen(3000, () => {console.log("Server started on port 3000");})
+app.listen(3000, () => {
+  console.log("Server started on port 3000");
+})
