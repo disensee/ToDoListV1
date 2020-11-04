a11887e8a6728bd8b2a16976df19528e7d8a38dc
disensee <disensee27@gmail.com>
2020-11-04 17:12:48 -0600

    Set up app.js and list.ejs to pass data back and forth

--- app.js
+++ app.js
@@ -2,43 +2,30 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.urlencoded({extended:true}));
 app.set('view engine', 'ejs');
+
+var items = ["Buy food", "Cook food", "Eat food"];
 
 app.get("/", (req, res) => {
   var today = new Date();
-  var currentDay = today.getDay();
-  var day = "";
+  var options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  };
 
-  switch (currentDay) {
-    case 0:
-      day = "Sunday";
-      break;
-    case 1:
-      day = "Monday";
-      break;
-    case 2:
-      day = "Tuesday";
-      break;
-    case 3:
-      day = "Wednesday";
-      break;
-    case 4:
-      day = "Thursday";
-      break;
-    case 5:
-      day = "Friday";
-      break;
-    case 6:
-      day = "Saturday";
-      break;
-    default:
-    console.log("ERROR: Current Day is equal to: " + currentDay);
+  var day = today.toLocaleDateString("en-US", options);
 
-  }
-  
   res.render('list', {
-    day: day
+    date: day,
+    newListItems: items
   });
+});
+
+app.post("/", (req, res) => {
+  items.push(req.body.toDoListItem);
+  res.redirect("/");
 });
 
 app.listen(3000, () => {
